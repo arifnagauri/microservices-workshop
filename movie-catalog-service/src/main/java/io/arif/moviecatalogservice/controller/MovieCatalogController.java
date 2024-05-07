1b94e363d4faec436eba68ab9103d16a6d34e193
Mohammed Arif <2017uee1663@mnit.ac.in>
2024-05-07 15:38:46 +0530

    test MovieCatalogService

---
+++ movie-catalog-service/src/main/java/io/arif/moviecatalogservice/controller/MovieCatalogController.java
@@ -0,0 +1,22 @@
+package io.arif.moviecatalogservice.controller;
+
+import io.arif.moviecatalogservice.model.CatalogItem;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogController {
+
+    @GetMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") int userId) {
+        return Collections.singletonList(
+                new CatalogItem("Titanic", "Test", 4)
+        );
+    }
+}
