16770556af546c8232c69489ec03253eb7c1cdde
Mohammed Arif <2017uee1663@mnit.ac.in>
2024-05-07 14:57:53 +0530

    Add Main SpringBoot Application Classes and gitignore file

---
+++ movie-catalog-service/src/main/java/io/arif/moviecatalogservice/MovieCatalogServiceApp.java
@@ -0,0 +1,12 @@
+package io.arif.moviecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatalogServiceApp.class, args);
+    }
+}
---
+++ ratings-data-service/src/main/java/io/arif/ratingsdataservcie/RatingsDataServiceApp.java
@@ -0,0 +1,12 @@
+package io.arif.ratingsdataservcie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingsDataServiceApp.class, args);
+    }
+}
