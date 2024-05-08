a768a12abde4aad6290e68ad68848d5933b4e90b
Mohammed Arif <2017uee1663@mnit.ac.in>
2024-05-08 15:56:37 +0530

    Add service discovery server // spring-cloud => Netflix-Eureka

---
+++ discovery-server/src/test/java/io/arif/dicoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.arif.dicoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
