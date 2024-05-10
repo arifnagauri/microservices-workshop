777112f5c3f4276fa101492836dcc661dddccbf8
Mohammed Arif <2017uee1663@mnit.ac.in>
2024-05-10 12:35:13 +0530

    Make external calls to external service //To learn Fault Tolerance and Resilience

--- movie-info-service/src/main/java/io/arif/movieinfoservice/MovieInfoServiceApp.java
+++ movie-info-service/src/main/java/io/arif/movieinfoservice/MovieInfoServiceApp.java
@@ -2,9 +2,18 @@ package io.arif.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieInfoServiceApp {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MovieInfoServiceApp.class, args);
--- movie-info-service/src/main/java/io/arif/movieinfoservice/controller/MovieInfoController.java
+++ movie-info-service/src/main/java/io/arif/movieinfoservice/controller/MovieInfoController.java
@@ -1,17 +1,33 @@
 package io.arif.movieinfoservice.controller;
 
 import io.arif.movieinfoservice.model.Movie;
+import io.arif.movieinfoservice.model.MovieSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieInfoController {
 
+    @Value("${api.key}")
+    private String apiKey;
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    MovieInfoController(final RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @GetMapping("/{movieId}")
     public Movie getMovie(@PathVariable("movieId") String movieId) {
-        return new Movie(movieId, "test", "test desc");
+        MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId +
+                "?api_key=" +  apiKey, MovieSummary.class);
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
     }
 }
